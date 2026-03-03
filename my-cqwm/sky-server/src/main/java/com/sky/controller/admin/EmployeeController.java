8b6505b3c40d382a9ef82739f0e88c6f3ab55a06
yize xu <2637574910@qq.com>
2026-03-03 22:11:48 +0800

    导入分类管理功能

--- my-cqwm/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ my-cqwm/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -108,6 +108,20 @@ public class EmployeeController {
     }
 
     /**
+     * 根据id查询员工信息
+     * 
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工信息")
+    public Result<Employee> getById(@PathVariable("id") Long id) {
+        log.info("根据id查询员工信息,参数为：{}", id);
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    /**
      * 退出
      *
      * @return
@@ -118,4 +132,18 @@ public class EmployeeController {
         return Result.success();
     }
 
+    /**
+     * 更新员工信息
+     * 
+     * @param employeeDTO
+     * @return
+     */
+    // TODO
+    @PostMapping("/update")
+    @ApiOperation("更新员工信息")
+    public Result<?> update(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("更新员工信息:{}", employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
 }
